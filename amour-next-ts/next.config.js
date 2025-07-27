83f0c0ac65fb9c97062974d38dc4147691687bbc
lakshay bhatia <bhatia00lakshay@gmail.com>
2025-07-27 11:07:25 +0530

    deployment commit - 2

--- amour-next-ts/next.config.js
+++ amour-next-ts/next.config.js
@@ -1,16 +1,10 @@
-
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Your config options go here
   reactStrictMode: true,
   swcMinify: true,
-
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
   images: {
     domains: ['encrypted-tbn0.gstatic.com'],
   },
-
 };
 
 export default nextConfig;
